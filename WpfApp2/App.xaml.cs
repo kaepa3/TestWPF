d0e2a23f5b7d4500ad2fa77f71393a4017f64050
Takahiro Yamazaki <yamazaki@nvs-one.com>
2017-08-10 18:04:03 +0900

    作れば課題が見えてくる

--- WpfApp2/App.xaml.cs
+++ WpfApp2/App.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System.Windows;
 using NLog;
+using System.Threading.Tasks;
 
 namespace WpfApp2
 {
@@ -8,13 +9,29 @@ namespace WpfApp2
     /// </summary>
     public partial class App : Application
     {
-        private void Application_Startup(object sender, StartupEventArgs e)
+        private async void Application_Startup(object sender, StartupEventArgs e)
         {
-
             //  ログの初期化
             var log = LogManager.GetLogger("app_log");
             App1.Logging.Initialize(new WinlLog(log));
             App1.Logging.info("起動");
+            var mainWindow = new MainWindow(); //←起動したい画面クラス名
+            Splash wnd = new Splash();
+            wnd.Show();
+            var result = await RunTaskSync(wnd);
+            wnd.Close();
+            mainWindow.Show();
+        }
+
+        public Task<string> RunTaskSync(Splash wnd)
+        {
+            return Task.Run<string>(() => Run(wnd));
+        }
+
+        public string Run(Splash wnd)
+        {
+            System.Threading.Thread.Sleep(1000);
+            return "kido";
         }
     }
 }
